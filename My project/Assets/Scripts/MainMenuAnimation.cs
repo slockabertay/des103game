7cb46db69bca4922eba98b41f8b0f1dcf2eadbd5
SamL <sam.a.lock@gmail.com>
2023-01-05 19:14:34 +0000

    main menu animation

--- My project/Assets/Scripts/MainMenuAnimation.cs
+++ My project/Assets/Scripts/MainMenuAnimation.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class MainMenuAnimation : MonoBehaviour
 {
+    public Rigidbody2D sea;
     // Start is called before the first frame update
     void Start()
     {
-        
+        sea = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        sea.velocity = new Vector2(.1f, sea.velocity.y);
+
+        if(sea.position.x > 7.44f)
+        {
+            sea.position = new Vector2(-10.5f, .45f);
+        }
     }
 }
